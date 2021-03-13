c126410beca3287a701bc05fd3b86ac5c2f50821
aveltras <romain.viallard@outlook.fr>
2021-03-13 15:37:07 +0100

    Day 3 - Part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -2,15 +2,29 @@ use std::fs;
 use std::str::FromStr;
 
 pub fn solve() -> Result<(usize, usize), &'static str> {
-    let part1 = solve_part1()?;
-    let part2 = solve_part2()?;
+    let contents = fs::read_to_string("src/day03/input.txt").unwrap();
+    let tree_area: TreeArea = contents.parse().unwrap();
+
+    let part1 = solve_part1(&tree_area)?;
+    let part2 = solve_part2(&tree_area)?;
+
     Ok((part1, part2))
 }
 
-pub fn solve_part1() -> Result<usize, &'static str> {
-    let contents = fs::read_to_string("src/day03/input.txt").unwrap();
-    let tree_area: TreeArea = contents.parse().unwrap();
-    println!("{:?}", tree_area.locations[0]);
+fn solve_part1(tree_area: &TreeArea) -> Result<usize, &'static str> {
+    let collisions = collisions_per_slope(tree_area, &(3, 1));
+    Ok(collisions)
+}
+
+fn solve_part2(tree_area: &TreeArea) -> Result<usize, &'static str> {
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    Ok(slopes
+        .iter()
+        .map(|slope| collisions_per_slope(tree_area, slope))
+        .product())
+}
+
+fn collisions_per_slope(tree_area: &TreeArea, (x, y): &(usize, usize)) -> usize {
     let mut collisions = 0;
     let mut pos_x = 0;
     let mut pos_y = 0;
@@ -20,15 +34,11 @@ pub fn solve_part1() -> Result<usize, &'static str> {
         if row[pos_x % width] {
             collisions += 1;
         }
-        pos_y += 1;
-        pos_x += 3;
+        pos_x += x;
+        pos_y += y;
     }
 
-    Ok(collisions)
-}
-
-pub fn solve_part2() -> Result<usize, &'static str> {
-    Ok(2)
+    collisions
 }
 
 #[derive(Debug)]

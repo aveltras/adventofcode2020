319dae70861c09fdff565216ad5786a8134bf579
aveltras <romain.viallard@outlook.fr>
2021-03-14 12:52:37 +0100

    Day 6 - Part 2

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -4,13 +4,24 @@ pub fn solve() -> (usize, usize) {
     let input = fs::read_to_string("src/day06/input.txt").unwrap();
     let lines = input.split("\n\n");
 
-    let mut total = 0;
+    let mut total_anyone = 0;
+    let mut total_everyone = 0;
 
     for line in lines {
-        let mut answers = HashSet::<char>::new();
         let charset: HashSet<char> = line.chars().filter(|c| c.is_alphabetic()).collect();
-        total += charset.len();
+        total_anyone += charset.len();
+
+        let mut common: HashSet<char> = ('a'..='z').collect();
+
+        for answer in line.lines() {
+            common = common
+                .intersection(&answer.chars().collect())
+                .copied()
+                .collect();
+        }
+
+        total_everyone += common.len();
     }
 
-    (total, 1)
+    (total_anyone, total_everyone)
 }
